c20b976ff126df5cfbe04585662e24fb8739dfcf
eslazyk10 <eslazy32@gmail.com>
2021-06-07 15:05:59 -0500

    imported api and created random number generating function

--- src/General.js
+++ src/General.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import axios from "axios";
 
 function General() {
+
+const [count, setCount] = useState('')
+const options = {
+  method: 'GET',
+  url: 'https://dad-jokes.p.rapidapi.com/joke/type/general',
+  headers: {
+    'x-rapidapi-key': '71a33360f6mshf7992b774b0f347p18aedfjsnebef70c66ba8',
+    'x-rapidapi-host': 'dad-jokes.p.rapidapi.com'
+  }
+};
+  
+const max= (343);
+    function getRandomNum() {
+        setCount(Math.floor(Math.random() * (max + 1)))
+}
+
+axios.request(options).then(function(response) {
+    console.log(response.data.body[ count ].setup);
+    console.log(response.data.body[ count ].punchline);
+}).catch(function(error) {
+	console.error(error);
+});
+    
     return (
         <div>
             <Header />
-            hello from general
+            <h2>
+                Laughter is the best medicine!
+                <br />
+                Click her for your daily dose 😄
+            </h2>
+            <button onClick={ getRandomNum }>Wanna see something funny?</button>
+            <h4>{ count }</h4>
             <Footer />
         </div>
     )
